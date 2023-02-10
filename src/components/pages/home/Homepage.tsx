e37b11f0eeaebfcfd01b8deacf13dde625410576
Hiccstrid2019 <hiccup.colin@yandex.ru>
2023-02-10 21:47:53 +0300

    added content to the homepage

--- src/components/pages/home/Homepage.tsx
+++ src/components/pages/home/Homepage.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import classes from './Homepage.module.css'
+import Button from "../../ui/Button/Button";
+import {Link} from "react-router-dom";
+import Wave from './wave.svg';
 
 const Homepage = () => {
     return (
-        <div>
-
+        <div className={classes.container}>
+            <img src={Wave} className={classes.wave}/>
+            <div className={classes.main}>
+                Welcome to the
+                <div className={classes.title}>«Audio Storage»</div> - your personal, private audio storage
+                <p className={classes.p}>Record your thoughts, songs and get access from any device</p>
+                <Link to={'/project'}>
+                    <Button className={classes.go} text={'Try It'}/>
+                </Link>
+            </div>
+            <img src={Wave} className={classes.wave} style={{transform: "scale(-1,1)"}}/>
         </div>
     );
 };
